241ba2f83e6e254e0a2291a5f3c2fdc2a56f11c5
toiletbril <s9und@outlook.com>
2025-05-25 01:03:51 +0300

    iterate over valid unicode only

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,5 @@
-const GPA = std.heap.smp_allocator;
-var _WORD_ARENA = std.heap.ArenaAllocator.init(GPA);
-const WORD_ARENA = _WORD_ARENA.allocator();
+var _GPA = std.heap.GeneralPurposeAllocator(.{}).init;
+const GPA = _GPA.allocator();
 
 var STDERR = std.io.getStdErr().writer();
 var STDOUT = std.io.getStdOut().writer();
@@ -8,8 +7,8 @@ var STDOUT = std.io.getStdOut().writer();
 fn isDelimiter(b: u8) bool
 {
   switch (b) {
-    '"', '\'', '*', '+', '-', '%', '\\', '/', ' ', ',', '(', ')', '[', 0, ']',
-    '{', '}', '<', '>', '`', '|', '=', ':', ';', '\t', '\n', '\r'
+    '"', '\'', '*', '+', '-', '%', '\\', '/', ' ', ',', '(', ')', '[', ']', '{',
+    '}', '<', '>', '`', '|', '=', ':', ';', '\t', '\n', '\r', 0
       => return true,
     else => return false,
   }
@@ -24,7 +23,6 @@ fn addWordToWordMap(word_map: *WordMap, word: []const u8) !void
   entry.value_ptr.* += 1;
 }
 
-// TODO use a UTF8 iterator, just discard invalid bytes
 fn parseFileToWordMap(scratch_arena: *std.heap.ArenaAllocator,
                       word_map: *WordMap, file: *std.fs.File) !void
 {
@@ -35,20 +33,57 @@ fn parseFileToWordMap(scratch_arena: *std.heap.ArenaAllocator,
   var word_buf = try std.ArrayList(u8).initCapacity(scratch_allocator, 16);
   var file_reader = std.io.bufferedReader(file.reader());
   var n_read = try file_reader.read(read_buf.items);
+  var truncated_buf: [3]u8 = undefined;
+  var truncated_cp_sz = @as(u3, 0);
   while (n_read > 0) {
-    for (read_buf.items[0..n_read]) |b| {
-      if (isDelimiter(b)) {
+    var cp_start = @as(u64, 0);
+    while (cp_start < n_read) {
+      // 1. find out codepoint length.
+      const cp_sz =
+        std.unicode.utf8ByteSequenceLength(read_buf.items[cp_start]) catch {
+          cp_start += 1;
+          continue;
+        };
+      std.debug.assert(cp_sz <= 4);
+      const cp_end = cp_start + cp_sz;
+      // special case: codepoint was truncated and is partially present.
+      // remember the initial part and get out.
+      if (cp_end > n_read) {
+        std.debug.assert(cp_sz <= 3);
+        @memcpy(&truncated_buf, read_buf.items[cp_start..]);
+        truncated_cp_sz = cp_sz;
+        break;
+      }
+      // 2. retrieve the codepoint.
+      const cp = read_buf.items[cp_start..cp_end];
+      // 3. validate the codepoint.
+      if (!std.unicode.utf8ValidateSlice(cp)) {
+        cp_start += 1;
+        continue;
+      }
+      // 4. check whether codepoint is a delimiter.
+      if (isDelimiter(cp[0])) {
+        // add a word we collected to the word map.
         if (word_buf.items.len > 0) {
           try addWordToWordMap(word_map, word_buf.items[0..word_buf.items.len]);
           word_buf.clearRetainingCapacity();
         }
-        if (!std.ascii.isWhitespace(b))
-          try addWordToWordMap(word_map, &[1]u8{b});
-      } else if (!std.ascii.isControl(b)) {
-        try word_buf.append(b);
+        // add this delimiter as well.
+        if (!std.ascii.isWhitespace(cp[0]) and !std.ascii.isControl(cp[0]))
+          try addWordToWordMap(word_map, cp[0..1]);
+      } else if (!std.ascii.isControl(cp[0])) {
+        // append this codepoint to word buffer.
+        try word_buf.appendSlice(cp);
       }
+      cp_start += cp_sz;
     }
-    n_read = try file_reader.read(read_buf.items);
+    // read again. if the truncated buffer is not empty, put's it's contents in
+    // the beginning.
+    n_read = try file_reader.read(read_buf.items[truncated_cp_sz..]);
+    if (truncated_cp_sz > 0) {
+      @memcpy(read_buf.items, truncated_buf[0..truncated_cp_sz]);
+      truncated_cp_sz = 0;
+    }
   }
 }
 
@@ -61,9 +96,7 @@ fn sortWordMapEntries(word_map: *const WordMap) !std.ArrayList(WordMap.Entry)
 {
   var it = word_map.iterator();
   var sorted_wordmap = std.ArrayList(WordMap.Entry).init(GPA);
-  while (it.next()) |e| {
-    try sorted_wordmap.append(e);
-  }
+  while (it.next()) |e| try sorted_wordmap.append(e);
   std.mem.sort(WordMap.Entry, sorted_wordmap.items, {}, compareWordMapEntry);
   return sorted_wordmap;
 }
@@ -111,9 +144,7 @@ fn error_main() !void
   // skip program name.
   const program_name = args.next().?;
   while (args.next()) |arg| {
-    if (streq(arg, "--help")) {
-      return help(program_name);
-    }
+    if (streq(arg, "--help")) return help(program_name);
     // "-" is stdin.
     var file = if (streq(arg, "-"))
       std.io.getStdIn()
@@ -122,7 +153,7 @@ fn error_main() !void
     // preallocate some memory based on a file size.
     const stat = try file.stat();
     const estimated_entries: u32 =
-      @truncate(stat.size / @sizeOf(WordMap.KV) / 4);
+      @truncate(stat.size / @sizeOf(WordMap.Entry) / 4);
     try word_map.ensureTotalCapacity(estimated_entries);
     try parseFileToWordMap(&scratch_arena, &word_map, &file);
   }
@@ -131,9 +162,7 @@ fn error_main() !void
 
 pub fn main() !void
 {
-  error_main() catch |err| {
-    std.process.fatal("{any}.", .{ err });
-  };
+  error_main() catch |err| std.process.fatal("{any}.", .{ err });
   std.process.cleanExit();
 }
 
